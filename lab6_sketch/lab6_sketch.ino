cd76415189ea8818048d1dd61f9e204a534feff6
Jimmy!! <132008245+kuroifantaisie@users.noreply.github.com>
2024-02-20 15:32:33 -0600

    Added loops to verify peaks and measure time inbetween

--- lab6_sketch/lab6_sketch.ino
+++ lab6_sketch/lab6_sketch.ino
@@ -1,6 +1,13 @@
-const int sensor_pin = 0;
+const int sensor_pin = 1;
 int pulse_signal = 0;
 float BPM = 0;
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+int upper_threshold = A;
+int lower_threshold = B;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,5 +18,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   pulse_signal = analogRead(sensor_pin);
-  Serial.println(pulse_signal);
+  //Serial.println(pulse_signal);
+  delay(50)
+  if (pulse_signal > upper_threshold && any_peak_detected == false) {
+    any_peak_detected = true;
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+      first_peak_detected = true;
+    } 
+    else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+      first_peak_detected = false;
+    }
+    // Do something about this peak
+  }
+
+  if (pulse_signal < lower_threshold) {
+  any_peak_detected = false;
+  }
+  
+  Serial.print(pulse_period/60)
 }
