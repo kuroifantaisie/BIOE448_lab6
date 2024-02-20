3507636cb72aab8550842481f6b23ad02b39c51e
Jimmy!! <132008245+kuroifantaisie@users.noreply.github.com>
2024-02-20 15:00:02 -0600

    Added Serial Functions to Visualize Pulse Data

--- lab6_sketch/lab6_sketch.ino
+++ lab6_sketch/lab6_sketch.ino
@@ -4,10 +4,12 @@ float BPM = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  pulse_signal = analogRead(sensor_pin);
+  Serial.println(pulse_signal);
 }
