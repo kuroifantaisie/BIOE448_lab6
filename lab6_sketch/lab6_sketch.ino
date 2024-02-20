c1d43bb8e9ffed1650de920de677dbbca0fab751
Jimmy!! <132008245+kuroifantaisie@users.noreply.github.com>
2024-02-20 16:09:23 -0600

    Fixed Errors in Code

--- lab6_sketch/lab6_sketch.ino
+++ lab6_sketch/lab6_sketch.ino
@@ -1,13 +1,15 @@
 const int sensor_pin = 1;
 int pulse_signal = 0;
 float BPM = 0;
+float pulse_period = 0;
+
 bool any_peak_detected = false;
 bool first_peak_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
 
-int upper_threshold = A;
-int lower_threshold = B;
+int upper_threshold = 780;
+int lower_threshold = 765;
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,9 +21,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   pulse_signal = analogRead(sensor_pin);
   //Serial.println(pulse_signal);
-  delay(50)
+  delay(50);
+
   if (pulse_signal > upper_threshold && any_peak_detected == false) {
     any_peak_detected = true;
+    
     if (first_peak_detected == false) {
       first_pulse_time = millis();
       first_peak_detected = true;
@@ -37,6 +41,7 @@ void loop() {
   if (pulse_signal < lower_threshold) {
   any_peak_detected = false;
   }
-  
-  Serial.print(pulse_period/60)
+  BPM = 60000/pulse_period;
+
+  Serial.println(BPM);
 }
